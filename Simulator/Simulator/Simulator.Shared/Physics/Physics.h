1bc174ee2fc21695000a5276440b0f6153fadf35
William Emfinger <waemfinger@gmail.com>
2014-10-01 22:27:50 -0500

    Updating project
    * removed default DXApp code that gets generated
    * Adding in new code for app (currently not much) following the tutorial
    @ directxtutorial.com

--- Simulator/Simulator/Simulator.Shared/Physics/Physics.h
+++ Simulator/Simulator/Simulator.Shared/Physics/Physics.h
@@ -9,7 +9,7 @@ namespace Physics
 		template<typename T>
 		virtual void SetSimulationType(T SimulationType) = 0;
 
-		template<typename T>
-		virtual void StepObjects(std::vector<T>& Objects) = 0;
+		template<typename T1, typename T2>
+		virtual void StepObjects(std::vector<T1>& Objects, T2 deltaT) = 0;
 	};
 }
---
+++ Simulator/Simulator/Simulator.Shared/Simulator.cpp
@@ -0,0 +1,58 @@
+// Include the precompiled headers
+#include "pch.h"
+
+// Use some common namespaces to simplify the code
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Popups;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace Platform;
+
+
+// the class definition for the core "framework" of our app
+ref class App sealed : public IFrameworkView
+{
+public:
+	// some functions called by Windows
+	virtual void Initialize(CoreApplicationView^ AppView)
+	{
+		// set the OnActivated function to handle to Activated "event"
+		AppView->Activated += ref new TypedEventHandler
+			<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
+	}
+	virtual void SetWindow(CoreWindow^ Window) {}
+	virtual void Load(String^ EntryPoint) {}
+	virtual void Run() {}
+	virtual void Uninitialize() {}
+
+	// an "event" that is called when the application window is ready to be activated
+	void OnActivated(CoreApplicationView^ CoreAppView, IActivatedEventArgs^ Args)
+	{
+		CoreWindow^ Window = CoreWindow::GetForCurrentThread();
+		Window->Activate();
+	}
+};
+
+
+// the class definition that creates an instance of our core framework class
+ref class AppSource sealed : IFrameworkViewSource
+{
+public:
+	virtual IFrameworkView^ CreateView()
+	{
+		return ref new App();    // create an App class and return it
+	}
+};
+
+[MTAThread]    // define main() as a multi-threaded-apartment function
+
+// the starting point of all programs
+int main(Array<String^>^ args)
+{
+	CoreApplication::Run(ref new AppSource());    // create and run a new AppSource class
+	return 0;
+}
