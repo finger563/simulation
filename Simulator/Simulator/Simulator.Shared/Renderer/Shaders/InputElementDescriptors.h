619d1b4060af6a9ec98bfaf0b03755d183353221
William Emfinger <waemfinger@gmail.com>
2014-12-21 00:24:04 -0600

    fixed bug that caused linker error in new vs2013

--- Simulator/Simulator/Simulator.Shared/Renderer/Shaders/InputElementDescriptors.h
+++ Simulator/Simulator/Simulator.Shared/Renderer/Shaders/InputElementDescriptors.h
@@ -7,7 +7,7 @@ using namespace DirectX;
 namespace Renderer
 {
 	// Default Input Element Descriptor 
-	D3D11_INPUT_ELEMENT_DESC defaultIED[] =
+	static D3D11_INPUT_ELEMENT_DESC defaultIED[] =
 	{
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
